dc65feaa3003b93eca4c41e79fb4f4779c918f8b
william <william.frazao91@gmail.com>
2024-01-24 12:50:15 -0300

    criando a home e a lista de produtos via api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    
-  </div>
+  <section>
+    <h1>🤙 Compre ou Venda 👎</h1>
+    <ProdutosBuscar/>
+    <ProdutosLista/>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue"
+import ProdutosLista from "@/components/ProdutosLista.vue"
 
 export default {
   name: 'HomeView',
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
