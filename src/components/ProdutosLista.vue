48069eb08b9f6c51fa58b46f442179f560d6bdf0
william <william.frazao91@gmail.com>
2024-01-27 18:18:00 -0300

    WIP paginacao

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="produtos-container">
     <div v-if="produtos && produtos.length > 0" class="produtos">
-      <div class="produto" v-for="produto in produtos" :key="produto.id">
+      <div class="produto" v-for="(produto, index) in produtos" :key="index">
         <router-link to="/">
           <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
           <p class="preco">{{ produto.preco }}</p>
@@ -9,6 +9,8 @@
           <p>{{ produto.descricao }}</p>
         </router-link>
       </div>
+      <ProdutosPaginar :produtosTotal="produtosTotal"
+      :produtosPorPagina="produtosPorPagina"/>
   </div>
   <div v-else-if="produtos && produtos.length === 0">
     <p class="sem-resultados">Busca Vazia. Tente buscar outro termo.</p>
@@ -17,14 +19,20 @@
 </template>
 
 <script>
-import { api } from "@/services.js"
-import { serialize } from "@/helpers.js"
+import ProdutosPaginar from "@/components/ProdutosPaginar.vue";
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
 export default {
+  name: "ProdutosLista",
+  components: {
+    ProdutosPaginar
+  },  
   data() {
     return {
       produtos: null,
-      produtosPorPagina: 9
+      produtosPorPagina: 9,
+      produtosTotal: 0,
     };
   },
   computed: {
@@ -42,6 +50,7 @@ export default {
   methods: {
     getProdutos() {
       api.get(this.url).then(response => {
+        this.produtosTotal = Number(response.headers["x-total-count"]);
         this.produtos = response.data;
       })
     }
---
+++ src/components/ProdutosPaginar.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Paginas</p>
+</template>
+
+<script>
+export default {
+  props: {
+    produtosPorPagina: {
+      type: Number,
+      default: 1
+    },
+    produtosTotal: {
+      type: Number,
+      default: 1
+    },
+  },
+}
+</script>
+
+<style></style>
