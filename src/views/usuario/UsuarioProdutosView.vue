7843bbddb8ee779d99f249e863717916ec7bc699
william <william.frazao91@gmail.com>
2024-03-28 18:50:22 -0300

    usuario produto

--- src/views/usuario/UsuarioProdutosView.vue
+++ src/views/usuario/UsuarioProdutosView.vue
@@ -1,10 +1,50 @@
 <template>
-  <p>Lista de Produtos</p>
+  <section>
+    <h2>Adicionar Produtos</h2>
+    <ProdutoAdicionarView/>
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItemView :produto="produto">
+          <p>{{ produto.descricao }}</p>
+        </ProdutoItemView>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoAdicionarView from '@/components/ProdutoAdicionarView.vue';
+import ProdutoItemView from "@/components/ProdutoItemView.vue";
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionarView,
+    ProdutoItemView
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
 </style>
